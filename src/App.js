cfd15eeca70c366472cdc624fdfe1d95335205b4
iyaiyayo26 <fahiyanmr26@gmail.com>
2022-11-15 16:02:11 +0700

    betulin tag di App.js, dan Status.js

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
           <div>
             <img src=".\images\image-victor.jpg" alt="avatar" className="-my-14 rounded-full border-4 border-white  absolute m-auto left-0 right-0" />
           </div>
-          <p className="mt-16 mb-1 text-dark-grayish-blue">
+          <h3 className="mt-16 mb-1 text-dark-grayish-blue">
             <span className="text-very-dark-saturated-blue font-semibold">Victor Crest</span> 26
-          </p>
-          <p className="mb-5 text-dark-grayish-blue">London</p>
+          </h3>
+          <h3 className="mb-5 text-dark-grayish-blue">London</h3>
           {/* status: */}
           <div className="border-t border-dark-grayish-blue flex flex-row justify-center space-x-10 py-4">
             <Status parent="80k" child="Followers" />
--- src/Components/Status.js
+++ src/Components/Status.js
@@ -1,7 +1,7 @@
 export default function Status({ parent, child }) {
   return (
     <div>
-      <p className="font-bold text-lg text-very-dark-saturated-blue">{parent}</p>
+      <h3 className="font-bold text-lg text-very-dark-saturated-blue">{parent}</h3>
       <p className="text-xs font-light text-dark-grayish-blue">{child}</p>
     </div>
   );
